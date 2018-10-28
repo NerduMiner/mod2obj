86e73f3714140d96b74572732f07f69259ad6950
NerduMiner <keefer.ian@gmail.com>
2018-10-27 23:35:19 -0400

    More work on faces, vertices, and normals
    Program now removes more types of invalid data. Faces are more properly formatted to obj format.

--- modvertexread.py
+++ modvertexread.py
@@ -22,6 +22,9 @@ with open('0x10.bin', 'br') as f:
         except struct.error as err:
             break #eof
         #the following is for the more odd vertices I have found in navi.mod
+        #HARDCODE: Just in case we find a -0.0
+        if round(vertex[0],4) == -0.0:
+            continue
         '''
         if vertex[0] > 10 or vertex[0] < -10:
             continue
@@ -48,6 +51,9 @@ with open('0x11.bin', 'br') as g:
             normal = struct.unpack('>3f',data)
         except struct.error as err:
             break #eof
+        #HARDCODE: All 0.0 must go away
+        if round(normal[0],4) + round(normal[1],4) + round(normal[2],4) == 0.0:
+            continue
         index = str(round(normal[0],4)) + " " + str(round(normal[1],4)) + " " + str(round(normal[2],4))
         print(struct.unpack('>3f',data))
         obj.write("vn  " + str(index) + "\n")
@@ -82,32 +88,51 @@ print("0x30 finished extracting.")
 first = True
 repeats = []
 repeatNum = 0
-#This value is found multiple times in door.mod
-repeats.append([0,0,113,120])
+oldchk = ()
 with open('0x50.bin', 'br') as e:
     while e:
-        data = e.read(4)
+        data = e.read(6)
         try:
-            face = struct.unpack('>4B',data)
+            face = struct.unpack('>6B',data)
         except struct.error as err:
             break #eof
+        #HARDCODE: sometimes door.mod creates a 152 for face idk why
+        if face[0] == 152:
+            continue
+        if face[2] == 152:
+            continue
         #Check if the faces reference out of bound indexes
-        if face[0] > vertexNum:
+        for i in range(0,6,2):
+            if face[i] > vertexNum:
+                continue
+        #Is the entire face 0?
+        if face[0] + face[1] + face [2] + face [3] + face [4] + face [5]== 0:
             continue
-        if face[1] > vertexNum:
+        #Check if the faces referece out of bound normal indexes
+        for i in range(1,6,2):
+            if face[i] > normalNum:
+                continue
+        #Check for invalid faces
+        #Is the first vertex referenced again?
+        if face[0] == face[2]:
+            print("invalid face found")
             continue
-        if face[2] > vertexNum:
+        if face[0] == face[4]:
+            print("invalid face found")
             continue
-        if face[3] > vertexNum:
-            continue
-        if face[0] + face[1] + face [2] + face[3] == 0:
-            continue
-        face = sorted(face)
-        #Check for repeated faces
-        #initialize the first oldface
+        #Is the first vertex normal referenced again?
+        if face[1] == face[3]:
+            face = list(face)
+            face[1] = 1
+            face[3] = 2
+            face[5] = 3
+            face = tuple(face)
+        #create our tuple we will use to check for repeats
         if first == True:
             oldface = face
-        if face == oldface and not first:
+            oldchk = oldface
+        #Is previous face same as new one?
+        if face == oldchk:
             repeats.append(face)
             oldface = face
             first = False
@@ -116,7 +141,8 @@ with open('0x50.bin', 'br') as e:
             continue
         else:
             oldface = face
-        if face in repeats and not first:
+        #Is our new face a part of the repeat list?
+        if face in repeats:
             repeats.append(face)
             first = False
             oldface = face
@@ -125,16 +151,14 @@ with open('0x50.bin', 'br') as e:
             continue
         else:
             oldface = face
-        index = str(face[0]) + " " + str(face[1]) + " " + str(face[2]) + " " + str(face[3])
-        print(struct.unpack('>4B',data))
+        index = str(face[0]) + "//" + str(face[1]) + " " + str(face[2]) + "//" + str(face[3]) + " " + str(face[4]) + "//" + str(face[5])
+        print(struct.unpack('>6B',data))
         obj.write("f  " + str(index) + "\n")
         faceNum += 1
-        first = False
-#in the case that you are using 0x50 of door.mod
-obj.write("f  0 0 113 120\n")
 print("0x50 finished extracting.")
 print(str(vertexNum) + " vertices created.")
 print(str(normalNum) + " normals created.")
 print(str(faceNum) + " faces created.")
 print(str(repeatNum) + " repeat faces found.")
+print(first)
 print(repeats)
